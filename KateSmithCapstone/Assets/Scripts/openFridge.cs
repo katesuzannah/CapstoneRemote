f23ce1c7292ddaf8b7b5bfb28013521d94e93cee
Unknown <katesuzannah@gmail.com>
2017-12-09 01:09:12 -0500

    Outlet and fridge door script

---
+++ KateSmithCapstone/Assets/Scripts/openFridge.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openFridge : MonoBehaviour {
+
+    float openAngle = 143.626f;
+    float closedAngle = 0f;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Open();
+        }
+    }
+    public void Open() {
+        float angle = Mathf.LerpAngle(closedAngle, openAngle, Time.deltaTime);
+        transform.eulerAngles = new Vector3 (0f, angle, 0f);
+    }
+}
